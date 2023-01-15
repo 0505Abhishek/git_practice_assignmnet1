d8a8ec9255ab5a0d6a15bc836787c0c9bacf3206
Abhishek <abhisharma05052002@gmail.com>
2023-01-15 23:06:07 +0530

    prime number code

---
+++ prime.js
@@ -0,0 +1,13 @@
+let a=7;let count=0;
+for(let i=2;i<a/2;i++)
+{
+  if(a%i===0)
+ {
+   count=1;
+  console.log("Not a prime number");
+ }
+if(count===0)
+{
+console.log("It is prime number");
+}
+}
