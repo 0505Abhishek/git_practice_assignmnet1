311cef849a80f5d834d31ff68ad57bbc939d2ccb
Jatinkumar007 <jatin.kr9868@gmail.com>
2023-01-15 23:19:16 +0530

    problem solved

--- prime.js
+++ prime.js
@@ -4,10 +4,7 @@ for(let i=2;i<a/2;i++)
   if(a%i===0)
  {
    count=1;
-  console.log("Not a prime number");
  }
-if(count===0)
-{
-console.log("It is prime number");
 }
-}
+(count==0) ? console.log("prime") : console.log("Not prime");
+
